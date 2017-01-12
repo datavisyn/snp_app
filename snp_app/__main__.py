329de9df0ae81a9e933089660044af07654ce32f
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-12 22:43:09 +0100

    remove debug output

--- snp_app/__main__.py
+++ snp_app/__main__.py
@@ -1,3 +1,4 @@
 if __name__ == '__main__':
   from .server import app
+
   app.run(host='0.0.0.0', port=9000, debug=True)
--- snp_app/server.py
+++ snp_app/server.py
@@ -7,13 +7,15 @@ app = connexion.App(__name__, specification_dir='./')
 app.add_api('swagger.yaml', base_path='/api', arguments={'title': 'SNP Exploration API'})
 
 # host static web site
-static_page = Blueprint('static_page', 'static', static_folder=os.path.normpath(os.path.dirname(__file__) + '/../static'), static_url_path='')
+static_page = Blueprint('static_page', 'static',
+                        static_folder=os.path.normpath(os.path.dirname(__file__) + '/../static'), static_url_path='')
+
 
 @static_page.route('/')
 def root():
-  print('aa')
   return static_page.send_static_file('index.html')
+
 
 app.app.register_blueprint(static_page)
 
-print(app.app.url_map)
+#print(app.app.url_map)

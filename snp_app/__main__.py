150e149c1d43944b8e8293d4406c5abcf48ca42d
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-12 22:22:47 +0100

    enable flask debug flag

--- snp_app/__main__.py
+++ snp_app/__main__.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
   from .server import app
-  app.run(host='0.0.0.0', port=9000)
+  app.run(host='0.0.0.0', port=9000, debug=True)
--- snp_app/server.py
+++ snp_app/server.py
@@ -8,6 +8,7 @@ static_page = Blueprint('static_page', 'static', static_folder='static', static_
 
 @static_page.route('/')
 def root():
+  print 'aa'
   return static_page.send_static_file('index.html')
 
 app.app.register_blueprint(static_page)

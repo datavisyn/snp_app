a3053c3f338c522d169fe655879fd153962d1834
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-16 13:28:47 +0100

    refactor code to easily get all data

--- snp_app/default_controller.py
+++ snp_app/default_controller.py
@@ -3,6 +3,7 @@ import sqlite3
 import matplotlib
 import pandas as pd
 import flask
+from sys import maxint
 import os
 
 # http://stackoverflow.com/questions/2801882/generating-a-png-with-matplotlib-when-display-is-undefined#3054314
@@ -18,6 +19,7 @@ def get_db():
 
 
 def _to_abs_position(chromosome, location):
+  location = int(location)
   r = get_db().execute('SELECT shift, length FROM chromosome WHERE chr_name = ?', (chromosome,)).fetchone()
   shift = r[0]
   length = r[1]
@@ -80,7 +82,7 @@ def manhattan_get(width=None, height=None, geq_significance=None, plain=None):
 
   fig = plt.figure()
   ax = fig.add_subplot(111)
-  colors = ['0.9', '0.6'] # http://matplotlib.org/api/colors_api.html
+  colors = ['0.9', '0.6']  # http://matplotlib.org/api/colors_api.html
   x_labels = []
   x_labels_pos = []
 
@@ -88,8 +90,8 @@ def manhattan_get(width=None, height=None, geq_significance=None, plain=None):
 
   for num, chromosome in enumerate(chromosomes):
     group = pd.read_sql(
-        'select s.*, (s.chrom_start + c.shift) as abs_location from snp s left join chromosome c on s.chr_name = c.chr_name where pval <= ? and s.chr_name = ? order by abs_location',
-        params=(sig2pval(geq_significance) if geq_significance is not None else 1, chromosome.chr_name), con=get_db())
+      'select s.*, (s.chrom_start + c.shift) as abs_location from snp s left join chromosome c on s.chr_name = c.chr_name where pval <= ? and s.chr_name = ? order by abs_location',
+      params=(sig2pval(geq_significance) if geq_significance is not None else 1, chromosome.chr_name), con=get_db())
 
     # -log_10(pvalue)
     group['minuslog10pvalue'] = -np.log10(group.pval)
@@ -133,26 +135,47 @@ def manhattan_get(width=None, height=None, geq_significance=None, plain=None):
                            mimetype='image/png')
   return result
 
-def _to_query(from_chromosome, from_location, to_chromosome, to_location, geq_significance=None, leq_significance=None):
-  abs_from = _to_abs_position(from_chromosome, from_location)
-  abs_to = _to_abs_position(to_chromosome, to_location)
-  query_from_where = 'from snp s left join chromosome c on s.chr_name = c.chr_name where  (s.chrom_start + c.shift) between ? and ? and pval between ? and ?'
-  params = (abs_from, abs_to, 0 if leq_significance is None else sig2pval(leq_significance),
-          1 if geq_significance is None else sig2pval(geq_significance),)
-  return query_from_where, params
 
-def data_get(from_chromosome, from_location, to_chromosome, to_location, geq_significance=None, leq_significance=None):
+def _to_query(from_chromosome=None, from_location=0, to_chromosome=None, to_location=maxint, geq_significance=None,
+              leq_significance=None):
+  params = []
+  q = 'from snp s left join chromosome c on s.chr_name = c.chr_name '
+  append = 'where'
+  if to_chromosome is not None and from_chromosome is None:
+    abs_to = _to_abs_position(to_chromosome, to_location)
+    q += append + ' (s.chrom_start + c.shift) <= ?'
+    append = 'and'
+    params.append(abs_to)
+  elif from_chromosome is not None:
+    abs_from = _to_abs_position(from_chromosome, from_location)
+    abs_to = _to_abs_position(to_chromosome or from_chromosome, to_location)
+    q += append + ' (s.chrom_start + c.shift) between ? and ?'
+    append = 'and'
+    params.extend((abs_from, abs_to))
 
-  query_from_where, params = _to_query(from_chromosome, from_location, to_chromosome, to_location, geq_significance, leq_significance)
+  if geq_significance or leq_significance is not None:
+    q += append + ' (pval between ? and ?)'
+    append = 'and'
+    params.extend((0 if leq_significance is None else sig2pval(leq_significance),
+                   1 if geq_significance is None else sig2pval(geq_significance)))
 
-  query = 'select s.*, (s.chrom_start + c.shift) as abs_location '+query_from_where+' order by abs_location'
+  return q, params
+
+
+def data_get(from_chromosome=None, from_location=0, to_chromosome=None, to_location=maxint, geq_significance=None,
+             leq_significance=None):
+  query_from_where, params = _to_query(from_chromosome, from_location, to_chromosome, to_location, geq_significance,
+                                       leq_significance)
+
+  query = 'select s.*, (s.chrom_start + c.shift) as abs_location ' + query_from_where + ' order by abs_location'
   data = pd.read_sql(query, params=params, con=get_db())
 
   r = data.to_json(orient='records')
   return flask.Response(r, mimetype='application/json')
 
 
-def data_count_get(from_chromosome, from_location, to_chromosome, to_location, geq_significance=None, leq_significance=None):
+def data_count_get(from_chromosome=None, from_location=0, to_chromosome=None, to_location=maxint, geq_significance=None,
+                   leq_significance=None):
   query_from_where, params = _to_query(from_chromosome, from_location, to_chromosome, to_location, geq_significance,
                                        leq_significance)
 

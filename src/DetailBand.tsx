fb600b805b90b3ef81db8654ddecd53a3b6286b7
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-26 13:49:45 +0100

    fix detail band runtime error

--- src/DetailBand.tsx
+++ src/DetailBand.tsx
@@ -11,14 +11,22 @@ import {scaleLinear} from 'd3-scale';
 
 @observer
 export default class DetailBand extends React.Component<{state: AppState},{width: number}> {
+  constructor(props: {state: AppState}, context) {
+    super(props, context);
+
+    this.state = {
+      width: 1000
+    };
+  }
+
   render() {
     const {windowManhattan, windowLocusZoom} = this.props.state;
     if (windowManhattan == null || windowLocusZoom == null) {
       return;
     }
-    const width = this.state ? this.state.width : 1000;
-    const marginManhattan = { left: 32, right: 10};
-    const marginLocusZoom = { left: 50, right: 10};
+    const width = this.state.width;
+    const marginManhattan = {left: 32, right: 10};
+    const marginLocusZoom = {left: 50, right: 10};
 
     const xscale = scaleLinear()
       .domain(windowManhattan)
@@ -26,8 +34,15 @@ export default class DetailBand extends React.Component<{state: AppState},{width
 
     const xPos = xscale(windowLocusZoom[0]);
     const xPos2 = xscale(windowLocusZoom[1]);
-    return <svg className="detailBand" preserveAspectRatio="xMinYMax" viewBox={`0,0,${width},30`} height="30" width="100%" ref={(svg) => this.setState({width: (svg as SVGElement).clientWidth})}>
-      <path d={`M${xPos},0 L${marginLocusZoom.left},30 L${width-marginLocusZoom.right},30 L${xPos2},0 Z`} />
+    return <svg className="detailBand" preserveAspectRatio="xMinYMax" viewBox={`0,0,${width},30`} height="30"
+                width="100%" ref={(svg) => this.updateSize(svg as Element)}>
+      <path d={`M${xPos},0 L${marginLocusZoom.left},30 L${width-marginLocusZoom.right},30 L${xPos2},0 Z`}/>
     </svg>;
+  }
+
+  private updateSize(elem: Element) {
+    if (elem && elem.clientWidth !== this.state.width) {
+      this.setState({width: elem.clientWidth});
+    }
   }
 }
